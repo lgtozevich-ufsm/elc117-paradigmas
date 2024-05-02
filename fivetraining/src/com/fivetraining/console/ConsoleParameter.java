0fb03b86fe2c9db8dbceeb9b9227d56582f282ed
Jaime Filho <jaimeadanielfilho@hotmail.com>
2024-05-01 23:09:10 -0300

    Sorround date and date time hints in parentheses

--- fivetraining/src/com/fivetraining/console/ConsoleParameter.java
+++ fivetraining/src/com/fivetraining/console/ConsoleParameter.java
@@ -1,19 +1,18 @@
 package com.fivetraining.console;
 
-import com.fivetraining.console.exceptions.ConsoleCommandExecutionException;
 import com.fivetraining.console.exceptions.ConsoleCommandParsingException;
 import com.fivetraining.console.parsers.*;
 
 public class ConsoleParameter {
     private final ConsoleParser parser;
-    private final String type;
+    private final String hint;
 
     private final String name;
     private final boolean required;
 
-    private ConsoleParameter(ConsoleParser parser, String type, String name, boolean required) {
+    private ConsoleParameter(ConsoleParser parser, String hint, String name, boolean required) {
         this.parser = parser;
-        this.type = type;
+        this.hint = hint;
         this.name = name;
         this.required = required;
     }
@@ -44,9 +43,9 @@ public class ConsoleParameter {
 
     @Override
     public String toString() {
-        String fullName = this.name + ":" + this.type;
+        String nameWithHint = name + ":" + hint;
 
-        return isRequired() ? "<" + fullName + ">" : "[" + fullName + "]";
+        return isRequired() ? "<" + nameWithHint + ">" : "[" + nameWithHint + "]";
     }
 
     public static ConsoleParameter createInteger(String name, boolean required) {
@@ -66,10 +65,10 @@ public class ConsoleParameter {
     }
 
     public static ConsoleParameter createDate(String name, boolean required) {
-        return new ConsoleParameter(new DateParser(), "dd/MM/yyyy", name, required);
+        return new ConsoleParameter(new DateParser(), "(dd/MM/yyyy)", name, required);
     }
 
     public static ConsoleParameter createDateTime(String name, boolean required) {
-        return new ConsoleParameter(new DateTimeParser(), "dd/MM/yyyy HH:mm:ss", name, required);
+        return new ConsoleParameter(new DateTimeParser(), "(dd/MM/yyyy HH:mm:ss)", name, required);
     }
 }
