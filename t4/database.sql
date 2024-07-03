142decd317a9be1bdb3247b81b3557bc98b12cdc
Luis Gustavo Werle Tozevich <luisgustavowt@gmail.com>
2024-07-03 11:15:36 -0300

    feat:  add example table

---
+++ t4/database.sql
@@ -0,0 +1,17 @@
+create table example (
+    random_int int,
+    random_string varchar(255),
+    random_date date,
+    random_time time,
+    random_datetime datetime,
+    random_double double,
+    random_boolean boolean,
+    random_float float,
+    random_decimal decimal(10, 2),
+    random_text text,
+    random_varchar varchar(255),
+    random_char char(1),
+    random_bigint bigint,
+    random_smallint smallint,
+    primary_key int primary key auto_increment
+);
