cf32a7c8de4199d0a6ea279c8d958dcd16b10158
lgtozevich <lgtozevich@inf.ufsm.br>
2024-07-03 00:51:08 -0300

    feat: add TextDescriptor class for handling text type

---
+++ t4/src/engine/descriptors/TextDescriptor.java
@@ -0,0 +1,42 @@
+package engine.descriptors;
+import java.util.Random;
+
+public class TextDescriptor extends TypeDescriptor {
+    private static final int DEFAULT_TEXT_LENGTH = 20;
+    @Override
+    public String getSQLTypeName() {
+        return "TEXT";
+    }
+
+    @Override
+    public String getJavaTypeName() {
+        return "String";
+    }
+
+    @Override
+    public String getJdbcGetterMethodName() {
+        return "getString";
+    }
+
+    @Override
+    public String getJdbcSetterMethodName() {
+        return "setString";
+    }
+
+    @Override
+    public String getRandomValue() {
+
+        int length = DEFAULT_TEXT_LENGTH;
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ";
+
+        for (int i = 0; i < length; i++) {
+            char randomChar = characters.charAt(random.nextInt(characters.length()));
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
+    }
+}
