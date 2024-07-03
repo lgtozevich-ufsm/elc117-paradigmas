fb160f7e9ce1290e7366ef935556a72980bf15c2
lgtozevich <lgtozevich@inf.ufsm.br>
2024-07-03 00:51:18 -0300

    feat: add TimestampDescriptor class for handling timestamp type

---
+++ t4/src/engine/descriptors/TimestampDescriptor.java
@@ -0,0 +1,39 @@
+package engine.descriptors;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Random;
+
+public class TimestampDescriptor extends TypeDescriptor {
+    @Override
+    public String getSQLTypeName() {
+        return "TIMESTAMP";
+    }
+
+    @Override
+    public String getJavaTypeName() {
+        return "java.sql.Timestamp";
+    }
+
+    @Override
+    public String getJdbcGetterMethodName() {
+        return "getTimestamp";
+    }
+
+    @Override
+    public String getJdbcSetterMethodName() {
+        return "setTimestamp";
+    }
+
+    @Override
+    public String getRandomValue() {
+
+        LocalDateTime start = LocalDateTime.of(1970, 1, 1, 0, 0, 0);
+        long days = (long) (Math.random() * 365 * 50);
+        long seconds = (long) (Math.random() * 24 * 60 * 60); 
+        LocalDateTime randomDateTime = start.plusDays(days).plusSeconds(seconds);
+        
+        Timestamp randomTimestamp = Timestamp.valueOf(randomDateTime);
+        return randomTimestamp.toString();
+        
+    }
+}
