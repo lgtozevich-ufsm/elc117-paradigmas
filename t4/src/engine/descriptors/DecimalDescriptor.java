a68ca6c4eb519973b55f0235852c7379d055987a
lgtozevich <lgtozevich@inf.ufsm.br>
2024-07-03 00:49:20 -0300

    feat: add DecimalDescriptor class for handling decimal type

---
+++ t4/src/engine/descriptors/DecimalDescriptor.java
@@ -0,0 +1,48 @@
+package engine.descriptors;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
+public class DecimalDescriptor extends TypeDescriptor {
+    private final int size;
+
+    public DecimalDescriptor(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String getSQLTypeName() {
+        return "DECIMAL(" + size + ")";
+    }
+
+    @Override
+    public String getJavaTypeName() {
+        return "java.math.BigDecimal";
+    }
+
+    @Override
+    public String getJdbcGetterMethodName() {
+        return "getBigDecimal";
+    }
+
+    @Override
+    public String getJdbcSetterMethodName() {
+        return "setBigDecimal";
+    }
+
+    @Override
+    public String getRandomValue() {
+
+        Random random = new Random();
+
+        BigDecimal randomDecimal = new BigDecimal(random.nextDouble() * 1000); 
+
+        randomDecimal = randomDecimal.setScale(size, RoundingMode.HALF_UP);
+
+        return randomDecimal.toString();
+    }
+}
